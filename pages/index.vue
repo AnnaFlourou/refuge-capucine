22254b5577b3ced94802ed6f20d7795074ad3f77
AnnaFlourou <flourou.anna@gmail.com>
2019-10-06 17:49:31 +0200

    fisrt commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+     <video src="/video/Dog-in-Farm.mp4">
+      <!-- <p>Your browser does not support the video tag.</p> -->
+      <video id="example_video_1" class="video-js vjs-default-skin" controls     preload="none" width="100%" height="300" setup="{}">
+    <source src="/video/Dog-in-Farm.mp4" type="video/mp4">
+</video>
+    </video>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
